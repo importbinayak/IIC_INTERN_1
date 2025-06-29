af8ffaf3e6c039eca2c808a868737437fed61e0e
Binayak Dhungana <thedhungana478@gmail.com>
2025-06-29 12:23:22 +0545

    firstcommit

---
+++ day1.py
@@ -0,0 +1,190 @@
+# # a="binayak"
+# # b="dhungana"
+# # # print("my name is "+a+" "+b+" ans")
+# # print(f"my name is {a} {b}")
+# #operaters
+
+# #+,-,/,*,**,//,%
+# #control-flow if,for,while
+# #if-->conditional
+# # a=0
+# # if a<0:
+# #     print("negative")
+# # elif a>0:
+# #     print("positive")
+# # else:
+# #     print("zero")
+
+# # a=20
+# # input marks for 5 sub marks btn 100 and 0
+# # 1 fail display fail 
+# # sabai ma pass vako xa vane grade display garne
+
+# #neseted if
+
+# # a=15
+# # if a=="bijeta":
+# #     print("divisible by 2")
+# # if a%5==0:
+# #     print("divisible by 0 and 5")
+# # # else:
+# # #     print("cant")
+# # name=input("enter your name ")
+# # print(name)
+
+# # if name=="binayak":
+# #     print("you are god damn right")
+# # else:
+# #     print("the deal is closed")
+
+# # if:
+# #     if:
+# #         pass
+# #     else:
+# # # else:
+
+# # order=input("half or full")
+
+# # if order=="half":
+# #     price=50
+# # elif order=="full":
+# #     price=100
+    
+# # item=input("egg?")
+
+# # if item=="yes":
+# #     price +=30 #prce=price+30
+# #     print(f"your bill is {price}")
+# # else:
+# #     print(f"your bill is {price}")
+
+# #for -->> ::
+# # a="binayak"
+# # for i in a:
+# #     print(i)
+# # else:
+# #     print("loop completed")
+# #while
+# # a=5
+
+# # while a<6:
+# #     print(a)
+# #     a+=1
+
+# # #list
+# # lis=[1,32,144,32553,66,6534,7,7]
+# # print((lis))
+# # list1=[2,44,523,53]
+# # # lis.append(3)
+# # lis.insert(-1,"binayak")
+# # # lis.sort(reverse=True)
+# # lis.extend(list1)
+# # print(lis)
+# # list1=[]
+
+# #tuple-->()
+# # tup=(1,4,4423,432532,53,55352,55353523,44)
+# # list1=list(tup)
+# # list1[2]=4000
+# # list1.insert(1,444)
+# # tup=tuple(list1)
+# # print(tup)
+
+# #set-->{}
+
+# # set1={1,32,322,4,42,44,5}
+# # print(set1)
+
+# #dict-->{}
+# dict1={
+#     "name":"binayak",
+#     "name":"bardan",
+#     "age":16,
+#     "list":["binayak","bardan"],
+#     "dict":{
+#         "name":"drabesh"
+#     }
+# }
+# dict2={
+#     "name":"binayak",
+#     "name":"bardan",
+#     "age":16,
+#     "list":["binayak","bardan"],
+#     "dict":{
+#         "name":"drabesh"
+#     }
+# }
+# dict3={
+#     "1":dict1,
+#     "2":dict2
+# }
+# # del(dict1)
+# # dict1.clear()
+# # print(dict1)
+# # dict2=dict1.copy()
+# # a=dict1["list"]
+# # a.append("hanz")
+# # print(dict3["1"])
+
+#function
+
+# def func(**a):
+#     print("hello",a["abc"])
+
+# func(abc="hello")
+
+# def pal(number):
+#     return str(number)==str(number)[::-1]
+
+# num=int(input("enter a number"))
+# if pal(num):
+#     print("pal")
+# else:
+#     print("not ")
+
+# a="binayak"
+# def hello():
+#     global b
+#     b="hello"
+#     print(a,b)
+# hello()
+# print(b)
+
+#class
+# class animal:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+        
+#     def __str__(self):
+#         return self.name    
+    
+#     def human(self):
+#         print(f"{self.name} {self.age}")
+        
+# a=animal("binayak",16)
+# a.human()
+# print(a.name)
+
+#inheritance
+
+# class animal:
+#     def __init__(self,kingdom,clas=12):
+#         self.kingdom=kingdom
+#         self.clas=clas
+        
+# class person(animal):
+#      def __init__(self,name,kingdom):
+#          super().__init__(kingdom)
+#          self.name=name
+         
+
+# p=person(1,"hsgjh")
+# print(p.kingdom)
+# a=10
+# print(a)
+# print(b)
+
+
+    
+
